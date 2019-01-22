9c7e80b32d74914b701b15605993be00d5cecb88
Eduardo Jordan <eduardojordan@hotmail.com>
2019-01-22 20:44:58 +0100

    Api in TableView

---
+++ ClashOne/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  ClashOne
+//
+//  Created by Eduardo on 22/01/2019.
+//  Copyright © 2019 Eduardo Jordan Muñoz. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+
+// Datos codables
+struct DataApi: Codable{
+    var type: String?
+    var name: String?
+    var description: String?
+    var _id : String?
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    let API_URL = "http://www.clashapi.xyz/api/cards"
+    
+    var laData = [DataApi]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request(API_URL).responseJSON { response in
+            let json = response.data
+            do{
+                let decoder = JSONDecoder()
+                
+                self.laData = try decoder.decode([DataApi].self, from: json!)
+                if self.laData.count > 0{
+                self.tableView.reloadData()
+                   
+                }
+            }catch let err{
+                print(err)
+            }
+        }
+   }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return self.laData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        let dict = laData[indexPath.row]
+        
+        cell.textLabel?.text = dict.name
+        cell.detailTextLabel?.text = dict.type
+        
+        return cell
+    }
+    
+
+
+
+
+}
+
